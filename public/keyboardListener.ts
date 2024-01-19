5fc4f762147498ffaf0841d5c70c34f7cec6f61f
Caio M. S <caiocpe@gmail.com>
2024-01-19 01:17:22 -0300

    conversão de 'keyboardListener' para Typescript

---
+++ public/keyboardListener.ts
@@ -0,0 +1,52 @@
+import { Identifier, Observer } from "./game";
+
+export interface KeyboardListennerState{
+    observers: Observer[];
+    playerId: Identifier|null
+}
+
+export interface KeyboardListenner{
+    registerPlayerId: (arg: Identifier) => void;
+    subscribe: (arg: Observer) => void;
+}
+
+export default function createKeyboardListenner(document: Document): KeyboardListenner{
+    const state: KeyboardListennerState = {
+        observers: [],
+        playerId: null
+    }
+
+    function registerPlayerId(playerId: Identifier){
+        state.playerId = playerId
+    }
+
+    function subscribe(observeFunction: Observer){
+        state.observers.push(observeFunction)
+    }
+
+    function notifyAll(command: Record<string, any>){
+
+        for(const observeFunction of state.observers){
+            observeFunction(command)
+        }
+    }
+
+    function handleKeyDown(event: Record<string, any>){
+        const key = event.key;
+        const command = {
+            type: 'move-player',
+            playerId: state.playerId,
+            key,
+        }
+
+        notifyAll(command)
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+
+    return {
+        subscribe,
+        registerPlayerId,
+    }
+
+}
